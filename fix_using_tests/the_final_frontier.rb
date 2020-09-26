5b6bdaf5c73d4e798a75df6d82f0d53743debf27
Connor Mulholland <mulhollandconnor@icloud.com>
2020-09-26 06:00:27 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,5 +1,5 @@
 # don't forget to add: require 'pry'
-
+require 'pry'
 def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
@@ -12,4 +12,5 @@ end
 def engage
   star_date = generate_star_date
   state_log(star_date)
+  binding.pry
 end
