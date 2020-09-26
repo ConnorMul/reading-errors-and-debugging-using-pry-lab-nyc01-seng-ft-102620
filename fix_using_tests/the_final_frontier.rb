23c8836dc1b21bd4ef1800f3671460ff4f0d44d0
Connor Mulholland <mulhollandconnor@icloud.com>
2020-09-26 05:58:44 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -6,10 +6,10 @@ end
 
 
 def state_log(star_date)
+  star_date = generate_star_date
   "Captain's Log, #{star_date}."
 end
 
 def engage
-  star_date = generate_star_date
   puts state_log(star_date)
 end
